bb518bbe8f829c1df7796958d8bdf79df6684dc9
Abhishek Hulage <abhihulage14@gmail.com>
2024-04-23 18:45:12 +0530

    modified payments logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,9 @@ class PagesController < ApplicationController
   before_action :require_login, only: :home
 
   def index
-
   end
 
   def home
-
   end
 
   def user_profile
@@ -14,7 +12,6 @@ class PagesController < ApplicationController
   end
 
   def vendor_profile
-
   end
 
   def family_occasions
@@ -22,18 +19,18 @@ class PagesController < ApplicationController
   end
 
   def payment
-    @theme_name = params[:themeName]
+    @theme_name = params[:Name]
     @decorator_name = params[:decoratorName]
     @price = params[:price].to_i
 
-    @booking = Booking.new(theme_name: @theme_name, decorator_name: @decorator_name, price: @price, email: current_account.email)
+    @booking = Booking.new(theme: @theme_name, vendor_name: @decorator_name, price: @price, email: current_account.email)
     @booking.save
     redirect_to user_profile_path
   end
 
   def make_payment
     @booking = Booking.new(
-      theme: params[:themeName],
+      theme: params[:theme],
       vendor_name: params[:decoratorName],
       price: params[:price],
       email: current_user.email
@@ -42,11 +39,7 @@ class PagesController < ApplicationController
     if @booking.save
       redirect_to user_profile_path, notice: 'Booking was successfully created.'
     else
-      redirect_to payment_path(themeName: params[:themeName], decoratorName: params[:decoratorName], price: params[:price]), alert: 'Error creating booking.'
+      redirect_to payment_path(theme: params[:theme], decoratorName: params[:decoratorName], price: params[:price]), alert: 'Error creating booking.'
     end
   end
-
-
-
-
-  end
+end
